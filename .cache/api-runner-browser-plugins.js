b61c24d35bb2a45d0b91b774bd6ba0b6c54c5704
anton <antoni.wrobel@senstill.pl>
2020-09-21 21:09:45 +0200

    test

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-anchor-links/gatsby-browser.js'),
-      options: {"plugins":[],"offset":-90},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
-      options: {"plugins":[],"google":{"families":["Saira Semi Condensed:900,600,400","Acme"]}},
-    }]
